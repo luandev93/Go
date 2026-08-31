953ff969b07b75f6df9be1c0cdf9e486b2cf6ac8
luandev93 <luandev93@gmail.com>
2026-08-31 09:30:58 -0300

    chore: atualizar cache do ícone PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aprenda-go-v10';
+const CACHE='aprenda-go-v11';
 const CORE=['./','./index.html','./style.css','./app.js','./playlist.csv','./rust-playlist.csv','./postgresql-playlist.csv','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
